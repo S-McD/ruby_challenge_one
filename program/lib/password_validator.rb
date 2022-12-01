fa065b7228c5fa2cf0a24fc3fa067d9e0ecb0efc
Simone <95197307+S-McD@users.noreply.github.com>
2022-12-01 20:14:38 +0000

    ready to be reviewed

--- program/lib/password_validator.rb
+++ program/lib/password_validator.rb
@@ -3,4 +3,9 @@
 # - include at least one of these characters: !, @, $, %, &
 
 def valid?(password)
+    if (password.length >=8) && (password =~ /[!@$%&]/)
+        true
+    else
+        false
+    end    
 end
