a85daf48a8e1e76b22b6573a6f89d44c16b091a3
Simone <95197307+S-McD@users.noreply.github.com>
2022-12-02 15:03:17 +0000

    yet another solution

--- program/lib/password_validator.rb
+++ program/lib/password_validator.rb
@@ -1,15 +1,13 @@
 # Valid passowords are
 # - 8 or more characters
 # - include at least one of these characters: !, @, $, %, &
+=begin
+def valid?(password)
+if password.length > 7
+    if password.include?('!''@''£''$''%''&')
+end
+=end
 
 def valid?(password)
-    special = password =~ /[!@£$%&]/
-    length = password.length > 7
-    special && length ? true : false
+  (password.length >= 8) && (password =~ /[!@£$%&]/) ? true : false
 end
-
-=begin
-    special = password.include?('!''@''£''$''%''&')
-    length = password.length > 7
-    special && length ? true : false
-=end
