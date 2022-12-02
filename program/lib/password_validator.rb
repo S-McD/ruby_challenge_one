d02ab9edb3659e4895d96923d43341d0acf48c43
Simone <95197307+S-McD@users.noreply.github.com>
2022-12-02 12:55:16 +0000

    Rough working forhelping others

--- program/lib/password_validator.rb
+++ program/lib/password_validator.rb
@@ -3,4 +3,13 @@
 # - include at least one of these characters: !, @, $, %, &
 
 def valid?(password)
+    special = password =~ /[!@£$%&]/
+    length = password.length > 7
+    special && length ? true : false
 end
+
+=begin
+    special = password.include?('!''@''£''$''%''&')
+    length = password.length > 7
+    special && length ? true : false
+=end
